683637f69620bc144741538d99c3891fd4837fb2
Guilherme Barros <barros.guilherme21@gmail.com>
2023-12-21 05:03:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const api = fetch("http://localhost:3000/videos")
     videos.forEach((video)=>{
         containerVideos.innerHTML += `
         <li class="videos__item">
-            <iframe scr="${video.url} " title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
             <div class="descricao-video">
                 <img class="img-canal" src="${video.imagem}" alt= "logo do canal">
                 <h3 class="titulo-video">${video.titulo}</h3>
                 <p class="titulo-canal">${video.descricao}</p>
         </li>  
-    `}
-    ))
+    `
+}))
+
+.catch((error)=>
+    containerVideos.innerHTML = `<p> Houve um erro ao carregar os videos:  ${error}</p>`
+)
+
+    
+    
+    
