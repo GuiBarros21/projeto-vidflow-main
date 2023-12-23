9fca45ee2836d34ea4599ce37bfe030d48d0aa05
Guilherme Barros <barros.guilherme21@gmail.com>
2023-12-22 20:10:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -29,10 +29,15 @@ const api = fetch("http://localhost:3000/videos")
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
+    try {
   const busca = await fetch("http://localhost:3000/videos");
   const videos = await busca.json();
 
   videos.forEach((video) => {
+    if (video.categoria == "")
+  {
+    throw new Error('Video nao tem categoria')
+  }
     containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -43,6 +48,10 @@ async function buscarEMostrarVideos() {
         </li>  
     `;
   });
+    }
+    catch(error) {
+        containerVideos.innerHTML =`<p> Houve um erro ao carregar os videos: ${error}`;
+    }
 }
 
 buscarEMostrarVideos();
